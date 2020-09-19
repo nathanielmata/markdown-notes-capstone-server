7ce487cf2e62b409399bbe490718ade1ece2c16a
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-19 12:25:48 -0700

    add correct reference to the database connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const app = require('./app')
 const knex = require('knex')
-const { PORT, DATABASE_URL } = require('./config')
+const { PORT, DB_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
+  connection: DB_URL,
 })
 
 app.listen(PORT, () => {
