32fffa7812c6cb2bdd16d37291e506964c6ef146
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-19 12:22:50 -0700

    pull jwt secret from env vars

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://markdown@localhost/markdown',
   TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://markdown@localhost/markdown-test',
+  JWT_SECRET: process.env.JWT_SECRET || 'use-env-secret',
   CORS_WHITELIST: ['http://localhost:3000']
 }
