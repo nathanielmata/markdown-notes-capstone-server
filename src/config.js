aa30d13d5b84b1d4c92c1ea7d1700f20b51747f9
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-26 12:07:58 -0700

    change DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://markdown@localhost/markdown',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://markdown@localhost/markdown-test',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://markdown@localhost/markdown',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://markdown@localhost/markdown-test',
   JWT_SECRET: process.env.JWT_SECRET || 'use-env-secret',
   CORS_WHITELIST: ['http://localhost:3000']
 }
