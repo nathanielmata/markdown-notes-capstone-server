a80acce1ccc303c81db8ee0937083e9c284cbdfe
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-09-27 12:46:24 -0700

    add localhost client to cors whitelist

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://markdown@localhost/markdown',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://markdown@localhost/markdown-test',
   JWT_SECRET: process.env.JWT_SECRET || 'use-env-secret',
-  CORS_WHITELIST: ['http://localhost:3000']
+  CORS_WHITELIST: ['https://markdown-notes-capstone-client.vercel.app', 'http://localhost:3000/']
 }
